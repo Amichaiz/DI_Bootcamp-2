5b284558660e1c75fe93fc012922fa2a82559321
Amichai Berger <76897731+Amichaiz@users.noreply.github.com>
2022-05-08 17:16:50 +0300

    Update script.js

--- Week-8/Day-3/Daily-challange/script.js
+++ Week-8/Day-3/Daily-challange/script.js
@@ -23,3 +23,4 @@ rightTurn.push(e)
 console.log(leftTurn.length)
 console.log(rightTurn.length-leftTurn.length)
 })
+//next time you would be better of creating a variable counter and do plus plus here you create an array and make it bigger and bigger cpu wise this isnt the best approach keep up the good work
